29a1a2bd90d9fad9b40827922a63d6dfc0e11069
Harshsharma-ui <124657753+Harshsharma-ui@users.noreply.github.com>
2026-03-30 22:44:48 +0530

    Add files via upload

--- firebase.ts
+++ firebase.ts
@@ -1,10 +1,7 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+@import "tailwindcss";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+@theme {
+  --dark: class;
+}
+
+@variant dark (&:where(.dark, .dark *));
